d114b7ad7042fabda0deb7dbef69100f7319e106
HenryLi-0 <bxsci8266@gmail.com>
2026-03-28 12:38:22 -0400

    working!

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import cv2, numpy
 class Timelapse:
     def __init__(self):
         self.temp:Image = None
-        self.tray = tray
+        self.tray = None
 
         self.thread = None
         self.running = False
@@ -18,17 +18,15 @@ class Timelapse:
         self.time = None
         self.frames = 0
 
-        self.tray.title = "ready {}RSPF @{}FPTS".format(RSPF, FPTS)
-
     def start(self):
         if self.running: return
         self.time = str(math.floor(time.time()))
-        self.dir = TEMP_SAVE_DIR.join(self.time)
-        os.mkdir(self.dir)
+        self.dir = lambda x: TEMP_SAVE_DIR(self.time, x)
+        os.makedirs(self.dir(None), exist_ok=True)
 
         self.thread:Thread = Thread(target=self.snap, daemon=True)
-        self.thread.start()
         self.running = True
+        self.thread.start()
 
         self.tray.title = "live!"
 
@@ -36,11 +34,10 @@ class Timelapse:
         start = time.time()
         while self.running:
             temp = ImageGrab.grab()
-            temp = cv2.cvtColor(numpy.array(temp), cv2.COLOR_RGB2BGR)
-            temp.save(Image.fromarray(self.dir.join(TEMP_IMAGE_STYLE(self.frames))))
+            temp.save(self.dir(TEMP_IMAGE_STYLE(self.frames) + ".png"))
             self.temp = temp
             self.frames += 1
-            self.tray.title = "{} frames, {}s".format(self.frames, math.floor(time.time()-start))
+            self.tray.title = "{} frames, {}s".format(self.frames-1, math.floor(time.time()-start))
             time.sleep(RSPF)
 
     def stopReset(self):
@@ -48,11 +45,16 @@ class Timelapse:
         self.running = False
 
         if self.frames > 0:
-            height, width, layers = self.temp.shape
-            video = cv2.VideoWriter(self.time, cv2.VideoWriter_fourcc(*'mp4v'), FPTS, (width, height))
-            for i in range(self.frames):
-                video.write(cv2.imread(TEMP_IMAGE_STYLE(i)))
-                self.tray.title = "video {}/{} frames".format(i, self.frames)
+            os.makedirs(OUT_SAVE_DIR(None), exist_ok=True)
+            width, height = self.temp.size
+            video = cv2.VideoWriter(OUT_SAVE_DIR(self.time+".mp4"), cv2.VideoWriter_fourcc(*'mp4v'), FPTS, (width, height))
+            for i in range(self.frames-1):
+                frame = cv2.imread(self.dir(TEMP_IMAGE_STYLE(i) + ".png"))
+                if frame is None:
+                    print("missing frame {}!".format(i))
+                    continue
+                video.write(frame)
+                self.tray.title = "video {}/{} frames".format(i, self.frames-1)
             video.release()
             try: cv2.destroyAllWindows()
             except: pass
@@ -64,11 +66,15 @@ class Timelapse:
 timelapse = Timelapse()
 
 tray = Icon("Timelapse", TRAY_IMAGE)
+
 menu = Menu(
     MenuItem("Start", timelapse.start),
     MenuItem("Stop", timelapse.stopReset),
     MenuItem("Quit", tray.stop), 
 )
 tray.menu=menu
+
+tray.title = "ready {}RSPF @{}FPTS".format(RSPF, FPTS)
 timelapse.tray = tray
+
 tray.run()
--- settings.py
+++ settings.py
@@ -4,10 +4,11 @@ from PIL import Image
 TRAY_IMAGE_PATH = os.path.join("tray.png")
 TRAY_IMAGE = Image.open(TRAY_IMAGE_PATH).convert("RGBA")
 
-TEMP_SAVE_DIR = os.path.join("temp")
-TEMP_IMAGE_STYLE = lambda x: "frames_{:05f}".format(x)
+TEMP_SAVE_DIR = lambda x, y: os.path.join("temp", x) if y==None else os.path.join("temp", x, y)
+TEMP_IMAGE_STYLE = lambda x: "frames_{:05d}".format(x)
+OUT_SAVE_DIR = lambda x: os.path.join("out") if x==None else os.path.join("out", x)
 
 
 FPTS = 30 # frames per timelapse second
-RSPF = 2 # real seconds between frames
+RSPF = 1/FPTS # real seconds between frames
 print("SETTINGS: [INFO] 1 timelapse second is {} real seconds".format(RSPF*FPTS))
